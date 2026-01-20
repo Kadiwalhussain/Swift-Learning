42fc69366de4b6a4a1346aad769a9f3048fe4a7c
Hussain <hussainkadiwal100@gmail.com>
2026-01-20 12:18:16 +0530

    Initial Commit

---
+++ Hussain_testapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hussain_testapp
+//
+//  Created by Muhammad hussain Kadiwal on 1/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hussain_testappTests/Hussain_testappTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hussain_testappTests.swift
+//  Hussain_testappTests
+//
+//  Created by Muhammad hussain Kadiwal on 1/20/26.
+//
+
+import Testing
+@testable import Hussain_testapp
+
+struct Hussain_testappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
